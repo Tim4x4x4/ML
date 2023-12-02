71659bd618769276857ad89507d85ff6ab70a02e
unknown <you@example.com>
2023-12-03 01:06:17 +0800

    latest modify with features

--- Machine_learning_final_project.py
+++ Machine_learning_final_project.py
@@ -1,5 +1,6 @@
 import os
 import json
+import csv
 import numpy as np
 import pandas as pd
 import math
@@ -7,16 +8,30 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn import datasets
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 folder_path = r'C:\Users\Tim Chen\OneDrive\桌面\台大\台大機器學習\機器學習專案\html.2023.final.data\release'
+rain_path = 'C:/Users/Tim Chen/OneDrive/桌面/台大/台大機器學習/機器學習專案/extend X feature/weather/If_rain.xlsx'
+holiday_path = 'C:/Users/Tim Chen/OneDrive/桌面/台大/台大機器學習/機器學習專案/extend X feature/holiday/If_holiday.xlsx'
 
-import os
-import json
-
-def read_json_files(folder_path, stop_point):
+def read_feature(feature_path, feature_name):
+    df = pd.read_excel(feature_path)
+    df_dict = {}
+    for index, row in df.iterrows():
+        key = int(row['date'])
+        value = row[feature_name]
+        df_dict[str(key)] = value
+    return df_dict
+def read_json_files(folder_path, stop_point, rain_data, holiday_data):
     Date = []
     Data = []
     Bike_stops = []
@@ -30,7 +45,7 @@ def read_json_files(folder_path, stop_point):
                 break
             if file.endswith('.json'):
                 file_path = os.path.join(root, file)
-                
+  
                 with open(file_path, 'r', encoding='utf-8') as json_file:
                     data = json.load(json_file)
                     
@@ -43,7 +58,9 @@ def read_json_files(folder_path, stop_point):
                                 'total': value["tot"],
                                 'current': value["sbi"],
                                 'can_park': value["bemp"],
-                                'open': value["act"]
+                                'open': value["act"],
+                                'rain_hour': rain_data[f'{str(root)[-8:]}'],
+                                'holiday': holiday_data[f'{str(root)[-8:]}']
                             }
                             Data.append(entry)
                             
@@ -55,8 +72,9 @@ def read_json_files(folder_path, stop_point):
     
     return Data, Date, Bike_stops
 
-
-data, date, bike_stops = read_json_files(folder_path, 1)
+rain_data = read_feature(rain_path, "rain_hour")
+holiday_data = read_feature(holiday_path, "holiday")
+data, date, bike_stops = read_json_files(folder_path, 1, rain_data, holiday_data)
 new_data = {}
 
 for stops in bike_stops:
@@ -81,6 +99,7 @@ def err(y_test, y_pred, total_stop):
     sum /= y_test.size
     return sum
 stop_point = 0
+best_models = {}
 for stop in new_data:
     print(stop)
     X = []
@@ -90,24 +109,32 @@ for stop in new_data:
         total_stop = elem.get('total')
         if elem.get('open') == 0:
             continue
-        X.append([time_to_minutes(elem.get('time')), elem.get('total'), elem.get('open')])
+        X.append([time_to_minutes(elem.get('time')), elem.get('total'), elem.get('open'), elem.get('rain_hour'), elem.get('holiday')])
         y.append(elem.get('current'))
     X = np.array(X)
-    print(X.shape)
+    #print(X.shape)
     y = np.array(y)
     X_numeric = X.astype(float)
-    X_train, X_test, y_train, y_test = train_test_split(X_numeric, y, test_size=0.1, random_state=42)
-    
-    model = SVC(kernel='linear') 
-    model.fit(X_train, y_train)
-    
-    y_pred = model.predict(X_test)
-    #print(y_test)
-    #print(y_pred)  
-    error = err(y_test, y_pred, total_stop)
-    print(error)
-       
+    smallest_error = float('inf')
+    best_model = RandomForestClassifier(n_estimators=100, max_depth = 10, random_state=42) #initialize
+    for i in range(10):
+        X_train, X_test, y_train, y_test = train_test_split(X_numeric, y, test_size=0.1, random_state=i*42)
         
+        model = RandomForestClassifier(n_estimators=100, max_depth = 10, random_state=42)
+        model.fit(X_train, y_train)
+        
+        y_pred = model.predict(X_test)
+        #print(y_test)
+        #print(y_pred)  
+        #for true_value, predicted_value in zip(y_test, y_pred):
+            #print(f"True: {true_value}, Predicted: {predicted_value}")
+        error = err(y_test, y_pred, total_stop)
+        if error < smallest_error:
+            smallest_error = error
+            best_model = model
+    best_models[str(stop)] = best_model
+    print(smallest_error)
+           
 print("Done")
 
 
